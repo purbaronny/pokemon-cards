4c49aabc3e6ef6bba5771e8271eea0efe67c14e3
purbaronny <ronny.purba@yahoo.co.id>
2025-03-06 10:26:02 +0700

    c

--- src/hooks/usePokemonList.ts
+++ src/hooks/usePokemonList.ts
@@ -1,3 +1,16 @@
+// import { useContext } from "react";
+// import PokemonContext from "../context/PokemonContext";
+
+// const usePokemonList = () => {
+//   const context = useContext(PokemonContext);
+//   if (context === undefined) {
+//     throw new Error('usePokemonList must be used within a PokemonProvider');
+//   }
+//   return context;
+// };
+
+// export default usePokemonList;
+
 import { useContext, useEffect } from "react";
 import PokemonContext from "../context/PokemonContext";
 
@@ -7,6 +20,14 @@ const usePokemonList = () => {
   useEffect(() => {
     setPage(page); // Memastikan data diperbarui saat halaman berubah
   }, [page, setPage]);
+
+  useEffect(() => {
+    setSearchQuery(searchQuery);
+  }, [searchQuery, setSearchQuery]);
+
+  useEffect(() => {
+    setSortByField(sortByField);
+  }, [sortByField, setSortByField]);
 
   return { pokemonList, loading, error, searchQuery, sortByField, setSearchQuery, setSortByField, setPage, page };
 };
